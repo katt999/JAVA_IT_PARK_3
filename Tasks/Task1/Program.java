759337820b5d01363c3d85fb55b0fb650a28eec0
rails <rails>
2017-09-22 20:12:46 +0300

    refactor first task

--- Tasks/Task1/Program.java
+++ Tasks/Task1/Program.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
-		int x = 13;
-		int y = 15;
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
 		int sumResult = x + y;
 
 		System.out.println(sumResult);
