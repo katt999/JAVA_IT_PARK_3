ec584bd02e616418212ff07691ce10a1a982b2bc
katt999 <katt99@mail.ru>
2017-09-23 10:53:11 +0300

    New folder Task2

--- Tasks/Task2/Program.java
+++ Tasks/Task2/Program.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		
+		int number = scanner.nextInt();		
 		int x0 = number % 2;
 		number = number / 2;
 		int x1 = number % 2;
