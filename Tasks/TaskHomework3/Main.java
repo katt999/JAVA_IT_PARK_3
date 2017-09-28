bb026a9969b6db9a5b75c555205c50e9795641d3
katt999 <katt99@mail.ru>
2017-09-28 14:35:12 +0300

    midifed taskhomework3

--- Tasks/TaskHomework3/Main.java
+++ Tasks/TaskHomework3/Main.java
@@ -3,14 +3,15 @@
  class Main {
 
     public static void main(String[] args) {
-        int[] array = {2, 0, 4, 7, 8, 3, 10};       
+        int[] array = {2, 0, 4, 7, 8, 3, 10};     
+        //int[] array = {0, 0, 5, 7, 8, 11, 3};         
         int sumChet = 0;
         int sumNechet = 0;
         int sumChetPozicion = 0;
         int sumNechetPozicion = 0;
         int kolLokMax = 0;
-        int maxLenghtTemp = 1; 
-        int maxLenght = 1;  
+        int maxLenghtTemp = 0; 
+        int maxLenght = 0;  
 
         for (int i=0;i < array.length;i++) {
 
@@ -32,15 +33,15 @@
                 kolLokMax++; 
         }
 
-        for (int i=1;i < array.length-1;i++) {
+        for (int i=0;i < array.length-1;i++) {
               
-                if (array[i-1] < array[i]) {                    
+                if (array[i] < array[i+1]) {                    
                     maxLenghtTemp++;                    
                 }
                 else {                     
                     if (maxLenght < maxLenghtTemp)
-                       maxLenght = maxLenghtTemp; 
-                    maxLenghtTemp = 1;
+                       maxLenght = maxLenghtTemp+1; 
+                    maxLenghtTemp = 0;
                 }
 
         }   
