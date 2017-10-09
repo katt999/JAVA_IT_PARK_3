c0cd7353c2fe43c857503f22851b47adcfe8ad94
katt999 <katt99@mail.ru>
2017-10-09 15:03:55 +0300

    create homeworkTask5

--- Tasks/Homework/Task5/src/Document.java
+++ Tasks/Homework/Task5/src/Document.java
@@ -1,57 +1,47 @@
 /**
  * Created by EVZabinskaya on 04.10.2017.
  */
-
+// Класс документ - отдельный файл
+// от класса можно создавать ОБЪЕКТЫ
 public class Document {
     // каждый объект класса будет иметь свои копии
     // count и lines
     int count = 0;
-    String lines[][] = new String[5][3];
+    String lines[] = new String[3];
 
-    void showDocuments() {
-        for (int i = 0; i < 5; i++) {
+    void showDocument() {
+        for (int i = 0; i < lines.length; i++) {
             // тернарный условный оператор e ? a : b
-            System.out.println("Документ №" + i);
-            for (int j = 0; j < 3; j++) {
-                String lineToOut = lines[i][j] == null ? "_______" : lines[i][j];
-                System.out.println(j + ": " + lineToOut);
-            }
+            String lineToOut = lines[i] == null ? "_______" : lines[i];
+            System.out.println(i + ": " + lineToOut);
         }
     }
 
-    void showDocument(Integer str) {
-        for (int j = 0; j < 3; j++) {
-            // тернарный условный оператор e ? a : b
-            String lineToOut = lines[str][j] == null ? "_______" : lines[str][j];
-            System.out.println(j + ": " + lineToOut);
+    void addLineToBegin(String newLine) {
+        for (int i = count; i > 0; i--) {
+            lines[i] = lines[i - 1];
         }
-    }
-
-    void addLineToBegin(String newLine, Integer str) {
-        for (int j = count; j > 0; j--) {
-            lines[str][j] = lines[str][j - 1];
-        }
-        lines[str][0] = newLine;
+        lines[0] = newLine;
         count++;
     }
 
-    void addLineToEnd(String newLine, Integer str) {
-        lines[str][count] = newLine;
+    void addLineToEnd(String newLine) {
+        lines[count] = newLine;
         count++;
     }
 
-    void delLine(Integer positionLine, Integer str) {
-        for (int j = positionLine; j < count-1; j++) {
-            lines[str][j] = lines[str][j + 1];
+    void delLine(Integer positionLine) {
+        for (int i = positionLine; i < count-1; i++) {
+            lines[i] = lines[i + 1];
         }
-        lines[str][count-1] = null;
+        lines[count-1] = null;
     }
 
-    void replaceLine(Integer positionLine, String line, Integer str) {
-       lines[str][positionLine] = line;
+    void replaceLine(Integer positionLine, String line) {
+        lines[positionLine] = line;
     }
 
-    void clearLine(Integer positionLine, Integer str) {
-        lines[str][positionLine] = null;
+    void clearLine(Integer positionLine) {
+        lines[positionLine] = null;
     }
 }
--- Tasks/Homework/Task5/src/MainMenuForDocuments.java
+++ Tasks/Homework/Task5/src/MainMenuForDocuments.java
@@ -1,101 +1,45 @@
 /**
  * Created by EVZabinskaya on 04.10.2017.
  */
-import java.io.IOException;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class MainMenuForDocuments {
 
-    public static int doc(int numstr) {
-         Document document = new Document();
-        // Обеспечить функционал:
-        // добавить строку в конец, в начало, в заданную
-        // позицию, заменить строку, удалить строку, вывести весь документ
+    public static void main(String[] args) {
+        Document folder[] = new Document[5];
+        for (int i = 0; i < folder.length; i++) {
+            folder[i] =  new Document();
+        }
+        //Document document[] = new Document[3];
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            MenuDoc.showMenu();
+            Menu.showMainMenu();
 
-            int command1 = scanner.nextInt();
+            int commandDoc = scanner.nextInt();
 
-
-            switch (command1) {
+            switch (commandDoc) {
                 case 1: {
-                    document.showDocument(numstr);
+                    Folder.showFolder(folder);
                 }
                 break;
                 case 2: {
-                    if (document.count == document.lines.length) {
-                        System.err.println("Нет места");
-                        break;
-                    }
-                    System.out.println("Введите строку: ");
-                    String newLine = scanner.next();
-                    document.addLineToBegin(newLine, numstr);
+                    System.out.println("Введите номер документа: ");
+                    int numberDoc = scanner.nextInt();
+                    EditDocument.editDocument(folder[numberDoc]);
                 }
                 break;
                 case 3: {
-                    if (document.count == document.lines.length) {
-                        System.err.println("Нет места");
-                        break;
-                    }
-                    System.out.println("Введите строку: ");
-                    String newLine = scanner.next();
-                    document.addLineToEnd(newLine, numstr);
+                    System.out.println("Введите номер документа: ");
+                    int numberDoc = scanner.nextInt();
+                    Folder.clearDocument(folder, numberDoc);
                 }
                 break;
                 case 4: {
-                    System.out.println("Введите номер строки: ");
-                    Integer positionLine = scanner.nextInt();
-                    document.delLine(positionLine, numstr);
-                }
-                case 5: {
-                    System.out.println("Введите номер строки, которую нужно заменить: ");
-                    Integer positionLine = scanner.nextInt();
-                    System.out.println("Введите новую строку: ");
-                    String line = scanner.next();
-                    document.replaceLine(positionLine, line, numstr);
-                }
-                break;
-                case 6: {
-                    System.out.println("Введите номер строки: ");
-                    Integer positionLine = scanner.nextInt();
-                    document.clearLine(positionLine, numstr);
-                }
-                break;
-                case 7:
-                    MainMenuForDocuments.main(null);
-            }
-        }
-    }
-
-
-
-
-    public static void main(String[] args) {
-        Document documents = new Document();
-        int count = 0;
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-
-            Menu.showMenu();
-            int command = scanner.nextInt();
-
-            switch (command) {
-                case 1: {
-                    documents.showDocuments();
-                    break;
-                }
-                case 2: {
-                    System.out.println("Введите номер документа: ");
-                    Integer numberDocument = scanner.nextInt();
-                    doc(numberDocument);
-                break;
-                }
-
-
-                case 7:
                     System.exit(0);
-            }
+                }
+                break;
+             }
         }
-    }
+   }
 }
