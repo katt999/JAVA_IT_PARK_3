5122b6c2ffee053875066974f8bd94dbf64183fd
katt999 <katt99@mail.ru>
2017-11-03 21:21:46 +0300

    workwithdb

---
+++ Tasks/WorkWithDB/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        String name = "postgres";
+        String password = "Zaq12wsx";
+        String url = "jdbc:postgresql://localhost:5432/Zabinskaya_db";
+        Connection connection = DriverManager.getConnection(url, name, password);
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM owner");
+
+        while (resultSet.next()) {
+            System.out.print(resultSet.getInt("id"));
+            System.out.println(" " + resultSet.getString("name"));
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String ownerName = scanner.next();
+        int age  = scanner.nextInt();
+
+       // statement.executeUpdate("INSERT INTO owner (name,age) VALUES ('"+ ownerName +"',"+ age +")");
+
+        PreparedStatement preparedStatement = connection;
+
+    }
+}
